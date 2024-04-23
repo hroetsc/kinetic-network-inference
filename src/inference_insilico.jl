3489c70cef9e0850adb4815de0f73cef8a17949d
hroetsc <hroetsc@mpibpc.mpg.de>
2024-04-23 10:06:55 +0200

    inference optimisation

--- src/inference_insilico.jl
+++ src/inference_insilico.jl
@@ -16,61 +16,52 @@ using RCall
 using CSV
 using Serialization
 using BenchmarkTools
+using PDFmerger: append_pdf!
+using DataFrames
 
 Random.seed!(42)
 print(Threads.nthreads())
 
-protein_name = "IDH1_WT"
-OUTNAME = "jac_MHsampler_4"
+protein_name = "insilicoEX2"
+OUTNAME = "v2"
 
 folderN = "results/inference/"*protein_name*"/"*OUTNAME*"/"
 mkpath(folderN)
 
 # ----- INPUT -----
-R"load(paste0('results/graphs/IDH1_WT.RData'))" # TODO: make protein_name variable
+R"load(paste0('../aQUIRE-network/results/matrices/DATA_Ex2.RData'))" # TODO: make protein_name variable
 @rget DATA;
 
-X = Array{Union{Missing,Float64}}(DATA[:S] .+ 1)
+X = Array{Union{Missing,Float64}}([DATA[:S] .+ 1 (DATA[:S0].-DATA[:DeltaC0])[:,1] .+ 1])
 A = Matrix{Int64}(DATA[:A])
 B = Matrix{Int64}(DATA[:B])
-x0 = Array{Float64}(X[1,:,1])
+x0 = Array{Float64}(X[1,:])
 
-tp = Vector{Float64}(DATA[:timepoints])
-replicates = DATA[:replicates]
 species = DATA[:species]
 reactions = DATA[:reactions]
+p = reactions.rate
 
-s = length(species)
-r = length(reactions)
-paramNames = [reactions; "σ"]
+tp = [0.0, 1.0, 2.0, 3.0, 4.0]
+s = length(species)+1
+r = length(p)
+paramNames = [reactions.rate_name; "σ"]
 tspan = (minimum(tp),maximum(tp))
-nr = length(replicates)
+
 N = transpose(B-A)
 
 
 # ----- settings -----
 numParam = length(paramNames)
-Niter = 25000
-nChains = 3
+Niter = 250000
+nChains = 8
+noWarmup = 250
 
 # TODO: different prior for on and off rates
 mini = 0
 maxi = 100
-midi = fill(0.5, r)
-sigi = fill(0.5, r)
+midi = p
+sigi = fill(1.0, r)
 
-p0 = rand(r)
-
-# ---- sample from prior distribution
-# FIXME / TODO
-# @model function prior(Σ, k)
-#     Σ ~ InverseGamma(2, 3)
-#     k ~ Product([truncated(Normal(mu,sigma), mini, maxi) for (mu,sigma) in zip(midi,sigi)])
-#     return Σ, k
-# end
-
-# prior_sample = likelihood(missing, missing)
-# prior_sample()
 
 
 # ----- likelihood function -----
@@ -94,12 +85,12 @@ end
 
 du0 = copy(x0)
 u0 = copy(x0)
-jac_sparsity = Symbolics.jacobian_sparsity((du, u) -> massaction!(du, u, p0, 0.0), du0, u0)
+jac_sparsity = Symbolics.jacobian_sparsity((du, u) -> massaction!(du, u, p, 0.0), du0, u0)
 jacspy = spy(jac_sparsity,title="sparsity of Jacobian",markersize=1,colorbar=false, dpi = 600)
 savefig(jacspy, folderN*"jacobian_sparsity.png")
 
 f! = ODEFunction(massaction!, jac = jacobian!; jac_prototype = float.(jac_sparsity))
-problem_jac = ODEProblem(f!, x0, tspan, p0)
+problem_jac = ODEProblem(f!, x0, tspan, p)
 
 # @mtkbuild sys = modelingtoolkitize(problem_jac)
 # problem_mtk = ODEProblem(sys, [], tspan, jac=true, sparse=true)
@@ -124,14 +115,9 @@ problem_jac = ODEProblem(f!, x0, tspan, p0)
     # print(predicted.u)
 
     # calculate likelihood
-    # iterate replicates
-    for i in 1:nr
-        # iterate products
-        for (ii, pred) in enumerate(predicted)
-            s = X[ii,:,i]
-            k = findall(!ismissing, s) # remove missing vales
-            s[k] ~ MvNormal(vec(pred)[k], Σ^2 * I)
-        end
+    for (ii, pred) in enumerate(predicted)
+        s = X[:,ii]
+        s ~ MvNormal(vec(pred), Σ^2 * I)
     end
 
     return nothing
@@ -153,8 +139,16 @@ function diagnostics_and_save(myChains)
     CSV.write(folderN*"quantiles.csv", chain_dsc[:2])
 
     # plot chain
-    # chain_pl = plot(myChains, dpi = 600)
-    # savefig(chain_pl, folderN*"chain.pdf")
+    # rm(folderN*"chain.pdf")
+    # touch(folderN*"chain.pdf")
+    
+    nm = DataFrame(chain_dsc[:1]).parameters
+    chains = Array(myChains)
+    for i in 1:size(chains)[2]
+        p = plot(chains[:,i], title = nm[i] , dpi = 600)
+        savefig(p, "tmp.pdf")
+        append_pdf!(folderN*"chain.pdf", "tmp.pdf", create=true, cleanup=true)
+    end
 
     # TODO: residual plots
     # TODO: plot sampler https://turinglang.org/dev/docs/using-turing/sampler-viz
@@ -169,19 +163,23 @@ model = likelihood(X, problem_jac)
 # run inference
 # FIXME: NUTS AD type --> it is messing with the solver
 benchmark = @benchmark sample(model, MH(), MCMCThreads(), 1, 1; progress=true, save_state=true)
-myChains = sample(model, MH(), MCMCThreads(), Niter, nChains; progress=true, save_state=true)
+# myChains = sample(model, MH(), MCMCThreads(), Niter, nChains; progress=true, save_state=true)
+
+myChains = sample(model, NUTS(50, 0.65, adtype=AutoZygote()), MCMCThreads(), 100, nChains; progress=true, save_state=true)
 
 diagnostics_and_save(myChains)
 
 # repeat
 for N in 2:100
 
-    # open(folderN*"chain.jls", "r") do io
-    #     chains_reloaded = deserialize(io)
-    # end
+    chains_reloaded = nothing
+    chains_reloaded = open(folderN*"chain.jls", "r") do io
+        deserialize(io)
+    end
     
-    myChains = sample(model, SMC(), MCMCThreads(), N*Niter, nChains; progress=true, save_state=true, resume_from=myChains)
+    myChains = sample(model, MH(), MCMCThreads(), N*Niter, nChains; progress=true, save_state=true, resume_from=chains_reloaded)
     diagnostics_and_save(myChains)
 
 end
+
 
